21fce1a1fb29a2465f2a14e224cb9c280f1d3f83
Sander de Haan <darxander@gmail.com>
2022-12-05 19:30:18 +0100

    Changed resources location

--- src/main/java/adventOfCode/Main.java
+++ src/main/java/adventOfCode/Main.java
@@ -5,8 +5,6 @@ import adventOfCode.TwentyTwentyTwo.*;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.io.InvalidClassException;
-import java.nio.file.InvalidPathException;
 import java.nio.file.Path;
 
 public class Main
@@ -29,30 +27,30 @@ public class Main
         switch (dayToPick)
         {
             case 1:
-                DayOne dayOne = new DayOne("C:\\Users\\san-d\\IdeaProjects\\AdventOfCode\\src\\main\\resources\\Day1\\inputDay1.txt");
+                DayOne dayOne = new DayOne("C:\\Users\\san-d\\IdeaProjects\\AdventOfCode\\src\\main\\resources\\2022\\input2022.txt");
                 System.out.println(dayOne.findLargestXOrCrash(1));
                 System.out.println(dayOne.findLargestXOrCrash(3));
                 break;
             case 2:
-                DayTwo dayTwo = new DayTwo("C:\\Users\\san-d\\IdeaProjects\\AdventOfCode\\src\\main\\resources\\Day1\\inputDay2.txt");
+                DayTwo dayTwo = new DayTwo("C:\\Users\\san-d\\IdeaProjects\\AdventOfCode\\src\\main\\resources\\2022\\inputDay2.txt");
                 System.out.println(dayTwo.calculatePoints());
                 System.out.println(dayTwo.calculatePointsForReal());
                 break;
             case 3:
 
-                DayThree dayThree = new DayThree("C:\\Users\\san-d\\IdeaProjects\\AdventOfCode\\src\\main\\resources\\Day1\\inputDay3.txt");
+                DayThree dayThree = new DayThree("C:\\Users\\san-d\\IdeaProjects\\AdventOfCode\\src\\main\\resources\\2022\\inputDay3.txt");
                 System.out.println(dayThree.findCommonInRucksack());
                 System.out.println(dayThree.findBadgePriorities(3));
                 break;
             case 4:
-                DayFour dayFour = new DayFour("C:\\Users\\san-d\\IdeaProjects\\AdventOfCode\\src\\main\\resources\\Day1\\inputDay4.txt");
+                DayFour dayFour = new DayFour("C:\\Users\\san-d\\IdeaProjects\\AdventOfCode\\src\\main\\resources\\2022\\inputDay4.txt");
                 System.out.println(dayFour.numberOfContainingPairs());
                 System.out.println(dayFour.numberOfOverlappingPairs());
                 break;
             case 5:
-                DayFive dayFive = new DayFive("C:\\Users\\san-d\\IdeaProjects\\AdventOfCode\\src\\main\\resources\\Day1\\inputDay5.txt");
+                DayFive dayFive = new DayFive("C:\\Users\\san-d\\IdeaProjects\\AdventOfCode\\src\\main\\resources\\2022\\inputDay5.txt");
                 System.out.println(dayFive.crateMover9000());
-                dayFive = new DayFive("C:\\Users\\san-d\\IdeaProjects\\AdventOfCode\\src\\main\\resources\\Day1\\inputDay5.txt");
+                dayFive = new DayFive("C:\\Users\\san-d\\IdeaProjects\\AdventOfCode\\src\\main\\resources\\2022\\inputDay5.txt");
                 System.out.println(dayFive.crateMover9001());
                 break;
             default:
