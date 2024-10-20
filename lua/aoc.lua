dc575fee343712c98557511f8f5aa208bafa338b
Sander de Haan <darxander@gmail.com>
2024-10-20 12:40:03 +0200

    uncomment lua day 1

--- lua/aoc.lua
+++ lua/aoc.lua
@@ -7,7 +7,7 @@ local function execute(day, part, f, ...)
 	print("Day, " .. day .. " Part, " .. part .. ":\n   Exectution time: " .. d .. " " .. u .. "\n   Result: " .. r)
 end
 
--- execute(1, 1, day1.part1, "../input/day1.txt")
--- execute(1, 2, day1.part2, "../input/day1.txt")
+execute(1, 1, day1.part1, "../input/day1.txt")
+execute(1, 2, day1.part2, "../input/day1.txt")
 execute(2, 1, day2.part1, "../input/day2.txt")
 execute(2, 2, day2.part2, "../input/day2.txt")
