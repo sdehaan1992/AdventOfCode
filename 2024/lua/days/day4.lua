be4772d0ea54fb265acc3dcbe4d5c513f3173664
Sander de Haan <darxander@gmail.com>
2024-12-04 13:14:09 +0100

    Added 2024 Day 4 in Lua

--- 2024/lua/days/day4.lua
+++ 2024/lua/days/day4.lua
@@ -7,53 +7,66 @@ local function get_dimensions(puzzle)
 	return string.len(puzzle) // linelength, linelength
 end
 
-local function perform_check(x, m, a, s)
-	print("checking xmas at :" .. m .. " " .. a .. " " .. s)
+local function perform_check(m, a, s)
 	if local_file:sub(m, m) == 'M' and local_file:sub(a, a) == 'A' and local_file:sub(s, s) == 'S' then
-		print("xmas at :" .. m .. " " .. a .. " " .. s)
 		return true
 	end
 	return false
 end
 
+local function x_mas(a)
+	local count = 0
+	if a > width and a < (height * width) - width and a % width > 1 and a % width < width - 1 then
+		if perform_check(a - (width - 1), a, a + (width + 1)) and perform_check(a - (width + 1), a, a + (width - 1)) or
+			perform_check(a + (width + 1), a, a - (width - 1)) and perform_check(a + (width - 1), a, a - (width + 1)) or
+			perform_check(a - (width + 1), a, a + (width + 1)) and perform_check(a + (width - 1), a, a - (width - 1)) or
+			perform_check(a + (width + 1), a, a - (width + 1)) and perform_check(a - (width - 1), a, a + (width - 1))
+		then
+			count = count + 1
+		end
+	end
+
+	return count
+end
+
 local function xmas(x)
 	local count = 0
 	if x % width < width - 3 then
-		-- if perform_check(x, x + 1, x + 2, x + 3) then
-		-- 	count = count + 1
-		-- end
+		if perform_check(x + 1, x + 2, x + 3) then
+			count = count + 1
+		end
 	end
 	if x % width > 3 then
-		-- if perform_check(x, x - 1, x - 2, x - 3) then
-		-- 	count = count + 1
-		-- end
+		if perform_check(x - 1, x - 2, x - 3) then
+			count = count + 1
+		end
 	end
 	if x > 3 * width then
-		-- if perform_check(x, x - width, x - width * 2, x - width * 3) then
-		-- 	count = count + 1
-		-- end
+		if perform_check(x - width, x - width * 2, x - width * 3) then
+			count = count + 1
+		end
 		if x % width > 3 then
-			if perform_check(x, x - (width - 1), x - (width - 2) * 2, x - (width - 3) * 3) then
+			if perform_check(x - (width + 1), x - (width + 1) * 2, x - (width + 1) * 3) then
 				count = count + 1
 			end
 		end
 		if x % width < width - 3 then
-			if perform_check(x, x - (width + 1), x - (width + 2) * 2, x - (width + 3) * 3) then
+			if perform_check(x - (width - 1), x - (width - 1) * 2, x - (width - 1) * 3) then
 				count = count + 1
 			end
 		end
 	end
 	if x < (height * width - (width * 3)) then
-		-- if perform_check(x, x + width, x + width * 2, x + width * 3) then
-		-- 	count = count + 1
-		-- end
+		if perform_check(x + width, x + width * 2, x + width * 3) then
+			count = count + 1
+		end
 		if x % width > 3 then
-			if perform_check(x, x + (width - 1), x + (width - 2) * 2, x + (width - 3) * 3) then
+			if perform_check(x + (width - 1), x + (width - 1) * 2, x + (width - 1) * 3) then
 				count = count + 1
 			end
 		end
 		if x % width < width - 3 then
-			if perform_check(x, x + (width + 1), x + (width + 2) * 2, x + (width + 3) * 3) then
+			if perform_check(x + (width + 1), x + (width + 1) * 2, x + (width + 1) * 3) then
 				count = count + 1
 			end
 		end
@@ -80,7 +93,20 @@ day4.part1 = function(file)
 end
 
 day4.part2 = function(file)
-	return calc(string.gsub(io.open(file, "r"):read("a"), "don't%(%).-do%(%)", ""):gsub("don't%(%).+", ""))
+	local result = 0
+	local_file = io.open(file, "r"):read("a")
+	height, width = get_dimensions(local_file)
+	local as = {}
+	local a = string.find(local_file, 'A', 1, true)
+	while a do
+		table.insert(as, a)
+		a = string.find(local_file, 'A', a + 1, true)
+	end
+
+	for _, loc in pairs(as) do
+		result = result + x_mas(loc)
+	end
+	return result
 end
 
 return day4
