5ef4a36f9b507bfe82c0d7ec19cc8ec72a9f8cdd
Sander de Haan <darxander@gmail.com>
2024-12-12 23:12:45 +0100

    Added 2024 Day 12 Part 2 in Lua

--- 2024/lua/days/day12.lua
+++ 2024/lua/days/day12.lua
@@ -1,6 +1,6 @@
 local day12 = {}
 
-local direction = { UP = 1, DOWN = 2, RIGHT = 3, LEFT = 4 }
+local plots, numrows, numcols, num_regions
 
 local function get_dimensions(grid)
 	local cols = string.find(grid, "\n", 1, true)
@@ -27,6 +27,36 @@ local function get_neighbours_locations(point, rows, cols)
 	return neighbours
 end
 
+local function get_directional_neighbours(point, rows, cols)
+	local neighbours = { u = nil, d = nil, l = nil, r = nil, lu = nil, ld = nil, ru = nil, rd = nil }
+	if point > cols then
+		neighbours.u = point - cols
+		if point % cols > 0 then
+			neighbours.ru = point - cols + 1
+		end
+		if (point - 1) % cols > 0 then
+			neighbours.lu = point - cols - 1
+		end
+	end
+	if point <= (rows * cols) - cols then
+		neighbours.d = point + cols
+		if point % cols > 0 then
+			neighbours.rd = point + cols + 1
+		end
+		if (point - 1) % cols > 0 then
+			neighbours.ld = point + cols - 1
+		end
+	end
+	if point % cols > 0 then
+		neighbours.r = point + 1
+	end
+	if (point - 1) % cols > 0 then
+		neighbours.l = point - 1
+	end
+
+	return neighbours
+end
+
 local function merge_regions(plots, region_into, region_from)
 	for _, plot in ipairs(plots) do
 		if plot.region == region_from then
@@ -37,13 +67,13 @@ end
 
 day12.part1 = function(file)
 	local grid = io.open(file, 'r'):read('a')
-	local rows, cols = get_dimensions(grid)
-	local plots = {}
+	numrows, numcols = get_dimensions(grid)
+	plots = {}
 	local regions = {}
-	local num_regions = 0
+	num_regions = 0
 	local input, _ = string.gsub(grid, "\n", "")
 	for i = 1, string.len(input) do
-		local neighbours = get_neighbours_locations(i, rows, cols)
+		local neighbours = get_neighbours_locations(i, numrows, numcols)
 		local plot = { type = string.sub(input, i, i), perimeter = (4 - #neighbours), region = 0 }
 		table.insert(plots, plot)
 		for idx, neighbour_idx in ipairs(neighbours) do
@@ -81,91 +111,71 @@ day12.part1 = function(file)
 end
 
 day12.part2 = function(file)
-	local grid = io.open(file, 'r'):read('a')
-	local rows, cols = get_dimensions(grid)
-	local plots = {}
-	local regions = {}
-	local num_regions = 0
-	local input, _ = string.gsub(grid, "\n", "")
-	for i = 1, string.len(input) do
-		local neighbours = get_neighbours_locations(i, rows, cols)
-		local plot = { type = string.sub(input, i, i), perimeter = (4 - #neighbours), region = 0, corners = 0 }
-		table.insert(plots, plot)
-		for _, neighbour_idx in ipairs(neighbours) do
-			if string.sub(input, neighbour_idx, neighbour_idx) ~= plot.type then
-				plot.perimeter = plot.perimeter + 1
-			else
-				if neighbour_idx < i then
-					if plots[neighbour_idx].region ~= plot.region then
-						merge_regions(plots, plots[neighbour_idx].region, plot.region)
-					end
-				end
-			end
-		end
-		if plot.region == 0 then
-			num_regions = num_regions + 1
-			plot.region = num_regions
-		end
+	if not plots then
+		day12.part1(file)
 	end
-
-	-- for _, region in ipairs(regions) do
-	-- 	local corners = 0
 	for plot_idx, plot in ipairs(plots) do
+		local neighbours = get_directional_neighbours(plot_idx, numrows, numcols)
+		local up, left, right, down, leftup, leftdown, rightup, rightdown
+		if neighbours.u then
+			up = plots[neighbours.u].type
+		end
+		if neighbours.l then
+			left = plots[neighbours.l].type
+		end
+		if neighbours.r then
+			right = plots[neighbours.r].type
+		end
+		if neighbours.d then
+			down = plots[neighbours.d].type
+		end
+		if neighbours.ld then
+			leftdown = plots[neighbours.ld].type
+		end
+		if neighbours.lu then
+			leftup = plots[neighbours.lu].type
+		end
+		if neighbours.ru then
+			rightup = plots[neighbours.ru].type
+		end
+		if neighbours.rd then
+			rightdown = plots[neighbours.rd].type
+		end
+
 		local corners = 0
-		local neighbours = get_neighbours_locations(plot_idx, rows, cols)
-		local regional_neighbours = {}
-		for _, neighbour in ipairs(neighbours) do
-			if plots[neighbour].region == plot.region then
-				table.insert(regional_neighbours, neighbour)
-			end
-		end
-		-- print("NEXT PLOT-----------------------------------------------------------")
-
-		local regional_edges = 4 - #regional_neighbours
-		-- print("Neighbour of plot of type: " .. plot.type .. " has " .. regional_edges .. " regional_edges")
-		if regional_edges == 2 then
-			if (plots[regional_neighbours[1]] == plots[plot_idx + 1] and plots[regional_neighbours[2]] == plots[plot_idx - 1]) or (plots[regional_neighbours[1]] == plots[plot_idx - cols] and plots[regional_neighbours[2]] == plots[plot_idx + cols]) then
-				corners = corners
-			else
+		if up == plot.type and left == plot.type then
+			if leftup ~= plot.type then
 				corners = corners + 1
-				if plots[regional_neighbours[1]] == plots[plot_idx - cols] then
-					if plots[regional_neighbours[2]] == plots[plot_idx + 1] then
-						if plots[plot_idx - cols + 1].type ~= plot.type then
-							corners = corners + 1
-							-- print("right top is not a regional neighbour")
-						end
-					elseif plots[regional_neighbours[2]] == plots[plot_idx - 1] then
-						if plots[plot_idx - cols - 1].type ~= plot.type then
-							corners = corners + 1
-							-- print("left top is not a regional neighbour")
-						end
-					end
-				elseif plots[regional_neighbours[1]] == plots[plot_idx + cols] then
-					if plots[regional_neighbours[2]] == plots[plot_idx + 1] then
-						if plots[plot_idx + cols + 1].type ~= plot.type then
-							corners = corners + 1
-							-- print("right bottom is not a regional neighbour")
-						end
-					elseif plots[regional_neighbours[2]] == plots[plot_idx - 1] then
-						if plots[plot_idx + cols - 1].type ~= plot.type then
-							corners = corners + 1
-							-- print("left bottom is not a regional neighbour")
-						end
-					end
-				end
 			end
-		elseif regional_edges == 3 then
-			corners = corners + 2
-		elseif regional_edges == 4 then
-			corners = corners + 4
+		elseif up ~= plot.type and left ~= plot.type then
+			corners = corners + 1
 		end
-		-- end
+		if up == plot.type and right == plot.type then
+			if rightup ~= plot.type then
+				corners = corners + 1
+			end
+		elseif up ~= plot.type and right ~= plot.type then
+			corners = corners + 1
+		end
+		if down == plot.type and right == plot.type then
+			if rightdown ~= plot.type then
+				corners = corners + 1
+			end
+		elseif down ~= plot.type and right ~= plot.type then
+			corners = corners + 1
+		end
+		if down == plot.type and left == plot.type then
+			if leftdown ~= plot.type then
+				corners = corners + 1
+			end
+		elseif down ~= plot.type and left ~= plot.type then
+			corners = corners + 1
+		end
 
-		-- print("Region has " .. corners .. " corners")
 		plot.corners = corners
-		print("setting corners to " .. plot.corners)
 	end
 
+	local regions = {}
 	for i = 1, num_regions do
 		regions[i] = { area = 0, corners = 0 }
 	end
@@ -176,7 +186,6 @@ day12.part2 = function(file)
 
 	local result = 0
 	for _, region in ipairs(regions) do
-		print("region has an area of " .. region.area .. " with " .. region.corners .. " corners")
 		result = result + (region.area * region.corners)
 	end
 
