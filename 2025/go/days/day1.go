501ebae3912308295fe4d5e503ed177045a00165
Sander de Haan <darxander@gmail.com>
2025-12-01 17:49:01 +0100

    Better day 1 part 2

--- 2025/go/days/day1.go
+++ 2025/go/days/day1.go
@@ -44,16 +44,21 @@ func part2(input []byte) {
 		line := scanner.Text()
 		matches := re.FindStringSubmatch(line)
 		value, _ := strconv.Atoi(matches[2])
-		increment := 1
+		ticks += value / 100
 		if matches[1] == "L" {
-			increment *= -1
-		}
-
-		for range value {
-			curr_value += increment
-			if curr_value%100 == 0 {
+			if value%100 >= curr_value && curr_value != 0 {
 				ticks++
 			}
+			value *= -1
+		} else {
+			if value%100+curr_value >= 100 {
+				ticks++
+			}
+		}
+
+		curr_value = (curr_value + value) % 100
+		if curr_value < 0 {
+			curr_value += 100
 		}
 	}
 
